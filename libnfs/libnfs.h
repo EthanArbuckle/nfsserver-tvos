e8d19e5c7875fb0dd58bb46e4e67f0e35ca17e02
ethanarbuckle <ethan.a.arbuckle@gmail.com>
2023-06-21 22:59:41 -0700

    Initial Commit

---
+++ libnfs/libnfs.h
@@ -0,0 +1,12 @@
+//
+//  libnfs.h
+//  libnfs
+//
+//  Created by Ethan Arbuckle on 6/21/23.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libnfs : NSObject
+
+@end
---
+++ libnfs/libnfs.m
@@ -0,0 +1,12 @@
+//
+//  libnfs.m
+//  libnfs
+//
+//  Created by Ethan Arbuckle on 6/21/23.
+//
+
+#import "libnfs.h"
+
+@implementation libnfs
+
+@end
