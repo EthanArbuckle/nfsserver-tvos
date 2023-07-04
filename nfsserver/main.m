88a81ad2dfcd3b702435cbf32b0f4a98bea751cd
ethanarbuckle <ethan.a.arbuckle@gmail.com>
2023-07-03 19:09:51 -0700

    start daemon

--- nfsserver/main.m
+++ nfsserver/main.m
@@ -4,15 +4,11 @@
 //
 //  Created by Ethan Arbuckle on 6/21/23.
 //
+#import "daemon.h"
 
-#import <UIKit/UIKit.h>
-#import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
-    NSString * appDelegateClassName;
-    @autoreleasepool {
-        // Setup code that might create autoreleased objects goes here.
-        appDelegateClassName = NSStringFromClass([AppDelegate class]);
-    }
-    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+    
+    daemon_start();
+    return 0;
 }
